85f8fc044157f78ec652b54f355f787c6007c520
Bob Lail <bob.lailfamily@gmail.com>
2016-05-14 19:57:05 -0500

    [improvement] Separated core.date.relative.future and core.date.relative.past from core.date.relative (30m)

---
+++ lib/attentive/entities/core/date/relative/past.rb
@@ -0,0 +1,24 @@
+require "attentive/entity"
+require "date"
+
+Attentive::Entity.define "core.date.relative.past",
+    "today",
+    "yesterday",
+    "{{core.date.wday}}",
+    "last {{core.date.wday}}" do |match|
+
+  today = Date.today
+
+  if match.matched?("core.date.wday")
+    wday = match["core.date.wday"]
+    days_since_wday = today.wday - wday
+    days_since_wday += 7 if days_since_wday < 0
+    today - days_since_wday
+  else
+    case match.to_s
+    when "today" then today
+    when "yesterday" then today - 1
+    else raise NotImplementedError, "Unrecognized match: #{match.to_s}"
+    end
+  end
+end
--- test/entities/core_date_relative_test.rb
+++ test/entities/core_date_relative_test.rb
@@ -1,21 +1,22 @@
 require "test_helper"
 
-class RelativeDateTest < Minitest::Test
+class CoreDateRelativeTest < Minitest::Test
   include Attentive::Test::Matching
 
   self.default_context = %i{conversation}
 
+  def setup
+    Timecop.freeze Date.new(2016, 4, 25)
+  end
+
+  def teardown
+    Timecop.return
+  end
+
+
 
   context "core.date.relative" do
-    setup do
-      Timecop.freeze Date.new(2016, 4, 25)
-    end
-
-    teardown do
-      Timecop.return
-    end
-
-    should "match weekdays" do
+    should "match weekdays as the next upcoming weekday" do
       assert_entity_matches "tuesday", as: Date.new(2016, 4, 26), entity: "core.date.relative"
     end
 
@@ -31,13 +32,64 @@ class RelativeDateTest < Minitest::Test
       assert_entity_matches "tomorrow", as: Date.new(2016, 4, 26), entity: "core.date.relative"
     end
 
-    should "match weekdays in next week" do
-      assert_entity_matches "next friday", as: Date.new(2016, 5, 6), entity: "core.date.relative"
-    end
-
     should "match weekdays in last week" do
       assert_entity_matches "last friday", as: Date.new(2016, 4, 22), entity: "core.date.relative"
     end
+
+    should "match weekdays in next week" do
+      assert_entity_matches "next friday", as: Date.new(2016, 5, 6), entity: "core.date.relative"
+    end
   end
+
+
+
+  context "core.date.relative.past" do
+    should "match weekdays as the most recent weekday" do
+      assert_entity_matches "tuesday", as: Date.new(2016, 4, 19), entity: "core.date.relative.past"
+      assert_entity_matches "friday", as: Date.new(2016, 4, 22), entity: "core.date.relative.past"
+    end
+
+    should "hear 'last Tuesday' the same as it hears just 'Tuesday'" do
+      assert_entity_matches "last tuesday", as: Date.new(2016, 4, 19), entity: "core.date.relative.past"
+    end
+
+    should "match today" do
+      assert_entity_matches "today", as: Date.new(2016, 4, 25), entity: "core.date.relative.past"
+    end
+
+    should "match yesterday" do
+      assert_entity_matches "yesterday", as: Date.new(2016, 4, 24), entity: "core.date.relative.past"
+    end
+
+    should "not match tomorrow" do
+      refute_entity_matches "tomorrow", entity: "core.date.relative.past"
+    end
+  end
+
+
+
+  context "core.date.relative.future" do
+    should "match weekdays as the next upcoming weekday" do
+      assert_entity_matches "sunday", as: Date.new(2016, 5, 1), entity: "core.date.relative.future"
+    end
+
+    should "match today" do
+      assert_entity_matches "today", as: Date.new(2016, 4, 25), entity: "core.date.relative.future"
+    end
+
+    should "not match yesterday" do
+      refute_entity_matches "yesterday", entity: "core.date.relative.future"
+    end
+
+    should "match tomorrow" do
+      assert_entity_matches "tomorrow", as: Date.new(2016, 4, 26), entity: "core.date.relative.future"
+    end
+
+    should "weekdays in next week" do
+      assert_entity_matches "next friday", as: Date.new(2016, 5, 6), entity: "core.date.relative.future"
+    end
+  end
+
+
 
 end
